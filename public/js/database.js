5f6f1715e73c7d017ca6181f9257b690a464e23f
Shawn <shmorganl14@gmail.com>
2022-06-28 15:42:19 -0400

    getting somewhere
    able to read from realtime database and display the tree

--- public/js/database.js
+++ public/js/database.js
@@ -1,17 +1,29 @@
-import { initializeApp } from "firebase/app";
-import { getDatabase } from "firebase/database";
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "https://www.gstatic.com/firebasejs/9.8.4/firebase-app.js";
+import { getAnalytics } from "https://www.gstatic.com/firebasejs/9.8.4/firebase-analytics.js";
+import { getDatabase, ref, set } from "https://www.gstatic.com/firebasejs/9.8.4/firebase-database.js";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
 
-// TODO: Replace the following with your app's Firebase project configuration
-// See: https://firebase.google.com/docs/web/learn-more#config-object
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  // ...
-  // The value of `databaseURL` depends on the location of the database
-  databaseURL: "https://mars-programming-skill-tree-default-rtdb.firebaseio.com/",
+  apiKey: "AIzaSyCKxWtqJJVZFoiS-lPqwBbI0qa2Yzkogew",
+  authDomain: "mars-programming-skill-tree.firebaseapp.com",
+  databaseURL: "https://mars-programming-skill-tree-default-rtdb.firebaseio.com",
+  projectId: "mars-programming-skill-tree",
+  storageBucket: "mars-programming-skill-tree.appspot.com",
+  messagingSenderId: "416879190456",
+  appId: "1:416879190456:web:04c7f4a4e44ca463086f4d",
+  measurementId: "G-FGJ4L37YZN"
 };
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
 
 
 // Initialize Realtime Database and get a reference to the service
 const database = getDatabase(app);
+
+
--- public/js/script.js
+++ public/js/script.js
@@ -1,133 +1,148 @@
 
-  var width = 0;
-  var height = 700;
-  var border = 100;
-  var nodesize = 40;
+var width = 0;
+var height = 700;
+var border = 100;
+var nodesize = 40;
 
-  var canvas = d3.select(".container").append("svg")
-    .append("g")
-    .attr("transform", "translate("+border+","+border+")");
+var canvas = d3.select(".container").append("svg")
+  .append("g")
+  .attr("transform", "translate("+border+","+border+")");
 
-  width = $("svg").parent().width();
-  d3.select("svg")
-    .attr("width", width)
-    .attr("height", height);
+width = $("svg").parent().width();
+d3.select("svg")
+  .attr("width", width)
+  .attr("height", height);
 
-  // FILTERS
-  var filter = canvas.append("defs")
-    .append("filter")
-      .attr("id", "glowing")
-      .attr("filterUnits", "userSpaceOnUse")
-      .attr("width", "150%")
-      .attr("height", "150%");
+// FILTERS
+var filter = canvas.append("defs")
+  .append("filter")
+    .attr("id", "glowing")
+    .attr("filterUnits", "userSpaceOnUse")
+    .attr("width", "150%")
+    .attr("height", "150%");
 
-  filter.append("feGaussianBlur")
-    .attr("in", "SourceAlpha")
-    .attr("stdDeviation", 2)
-    .attr("result", "blur");
+filter.append("feGaussianBlur")
+  .attr("in", "SourceAlpha")
+  .attr("stdDeviation", 2)
+  .attr("result", "blur");
 
-  filter.append("feOffset")
-    .attr("in", "blur")
-    .attr("dx", 0)
-    .attr("dy", 0)
-    .attr("result", "offsetBlur");
+filter.append("feOffset")
+  .attr("in", "blur")
+  .attr("dx", 0)
+  .attr("dy", 0)
+  .attr("result", "offsetBlur");
 
-  filter.append("feFlood")
-    .attr("flood-color", "#0067ff")
-    .attr("result", "color");
-  filter.append("feComposite")
-    .attr("in2", "offsetBlur")
-    .attr("operator", "in");
+filter.append("feFlood")
+  .attr("flood-color", "#0067ff")
+  .attr("result", "color");
+filter.append("feComposite")
+  .attr("in2", "offsetBlur")
+  .attr("operator", "in");
 
-  var feMerge = filter.append("feMerge");
-  feMerge.append("feMergeNode");
-  feMerge.append("feMergeNode")
-    .attr("in", "SourceGraphic");
+var feMerge = filter.append("feMerge");
+feMerge.append("feMergeNode");
+feMerge.append("feMergeNode")
+  .attr("in", "SourceGraphic");
 
-  // TREE DEFINITION
-  var tree = d3.layout.tree()
-    .nodeSize(nodesize)
-    .separation(function(a,b){return 10;})
-    .size([height-2*border,width-2*border]);
+// TREE DEFINITION
+var tree = d3.layout.tree()
+  .nodeSize(nodesize)
+  .separation(function(a,b){return 10;})
+  .size([height-2*border,width-2*border]);
 
-  d3.json("https://gist.githubusercontent.com/ShawnLi14/71f7ed4d4b226d19fcc8629be035fd02/raw/60670cb07740cd775aaca7f51af591c7d2c47094/skill-tree.json", function(data){
+/*var ref = firebase.database().ref();
+ref.on('value', function(snapshot) {
 
-    //alert(JSON.stringify(data));
+  var data = snapshot.val();
+  // draw your d3 vis here using the data
 
-    var nodes = tree.nodes(data);
-    var links = tree.links(nodes);
+});*/
+import { getDatabase, ref, child, get } from "https://www.gstatic.com/firebasejs/9.8.4/firebase-database.js";
 
-    // Unused
-    var diagonal = d3.svg.diagonal()
-      .projection(function(d){
-        return [d.y, d.x];
-      });
-
-    var step_line = d3.svg.line().interpolate("step")
-      .x(function(d){ return d.ly })
-      .y(function(d){ return d.lx });
-
-    function diagonal_step(d){
-      var points = [
-          {lx: d.source.x, ly: d.source.y},
-          {lx: d.target.x, ly: d.target.y}
-      ];
-      return step_line(points);
-    }
-
-    // EDGES
-    canvas.selectAll(".link")
-      .data(links)
-      .enter()
-      .append("path")
-        .attr("class", "link glowing")
-        .attr("stroke", "black")
-        .attr("fill", "none")
-        .attr("d", diagonal_step);
-
-    // NODES
-    var node = canvas.selectAll(".node")
-      .data(nodes)
-      .enter()
-      .append("g")
-        .attr("class", "node")
-        .attr("transform", function (d) {
-          return "translate(" +d.y+ "," +d.x+ ")";
+const dbRef = ref(getDatabase());
+get(child(dbRef, '0')).then((snapshot) => {
+  if (snapshot.exists()) {
+    var data = snapshot.val();
+      //alert(JSON.stringify(data));
+      var nodes = tree.nodes(data);
+      var links = tree.links(nodes);
+      // Unused
+      var diagonal = d3.svg.diagonal()
+        .projection(function(d){
+          return [d.y, d.x];
         });
 
-    node.append("circle")
-      .attr("r", nodesize/2)
-      .attr("class", "glowing")
-      .attr("fill", "blue");
+      var step_line = d3.svg.line().interpolate("step")
+        .x(function(d){ return d.ly })
+        .y(function(d){ return d.lx });
 
-    // ADDING TOOLTIPS
-    if (typeof tooltip !== 'undefined') {
-    // the variable is defined
-	node.on("mouseover", function(d){d3.select(this).classed("active", true ); return show_tooltip(d);})
-	    .on("mousemove", function(d){return move_tooltip(d);})
-	    .on("mouseout", function(d){d3.select(this).classed("active", false ); return hide_tooltip(d);});
-    }
+      function diagonal_step(d){
+        var points = [
+            {lx: d.source.x, ly: d.source.y},
+            {lx: d.target.x, ly: d.target.y}
+        ];
+        return step_line(points);
+      }
 
-    node.append("image")
-      .attr("xlink:href", function(d) { return d.completed ? "img/icons/skillCompleted.png" : "img/icons/skill.png"; })
-      .attr("class", "skill-img glowing")
-      .attr("x", "-"+nodesize/2+"px")
-      .attr("y", "-"+nodesize/2+"px")
-      .attr("width", nodesize+"px")
-      .attr("height", nodesize+"px");
+      // EDGES
+      canvas.selectAll(".link")
+        .data(links)
+        .enter()
+        .append("path")
+          .attr("class", "link glowing")
+          .attr("stroke", "black")
+          .attr("fill", "none")
+          .attr("d", diagonal_step);
+
+      // NODES
+      var node = canvas.selectAll(".node")
+        .data(nodes)
+        .enter()
+        .append("g")
+          .attr("class", "node")
+          .attr("transform", function (d) {
+            return "translate(" +d.y+ "," +d.x+ ")";
+          });
+
+      node.append("circle")
+        .attr("r", nodesize/2)
+        .attr("class", "glowing")
+        .attr("fill", "blue");
+
+      // ADDING TOOLTIPS
+      if (typeof tooltip !== 'undefined') {
+      // the variable is defined
+    node.on("mouseover", function(d){d3.select(this).classed("active", true ); return show_tooltip(d);})
+        .on("mousemove", function(d){return move_tooltip(d);})
+        .on("mouseout", function(d){d3.select(this).classed("active", false ); return hide_tooltip(d);});
+      }
+
+      node.append("image")
+        .attr("xlink:href", function(d) { return d.completed ? "img/icons/skillCompleted.png" : "img/icons/skill.png"; })
+        .attr("class", "skill-img glowing")
+        .attr("x", "-"+nodesize/2+"px")
+        .attr("y", "-"+nodesize/2+"px")
+        .attr("width", nodesize+"px")
+        .attr("height", nodesize+"px");
 
 
-    node.append("image")
-      .attr("xlink:href", function(d) { return "img/icons/"+d.icon+".png"; })
-      .attr("class", "skill-icon")
-      .attr("x", "-"+(nodesize/2-4)+"px")
-      .attr("y", "-"+(nodesize/2-4)+"px")
-      .attr("width", (nodesize-8)+"px")
-      .attr("height", (nodesize-8)+"px");
+      node.append("image")
+        .attr("xlink:href", function(d) { return "img/icons/"+d.icon+".png"; })
+        .attr("class", "skill-icon")
+        .attr("x", "-"+(nodesize/2-4)+"px")
+        .attr("y", "-"+(nodesize/2-4)+"px")
+        .attr("width", (nodesize-8)+"px")
+        .attr("height", (nodesize-8)+"px");
 
 
-    node.append("text")
-      .text(function(d) {
-        return d.name;
-      });
-  });
+      node.append("text")
+        .text(function(d) {
+          return d.name;
+        });
+  }
+  else {
+    console.log("No data available");
+  }
+}).catch((error) => {
+  console.error(error);
+});
